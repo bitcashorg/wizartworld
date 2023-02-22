4d631b59c168b092415feda82624fd4fdd02fe1c
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-21 21:24:27 -0600

    feat: add flow login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,30 @@
 import '~/styles/globals.css'
-
+import '../flow/config'
 import type { AppProps } from 'next/app'
 
 import { RootLayout } from '~/layouts/root'
 import { GlobalContextProvider } from '~/context/global.context'
 import { ReplicateProvider } from '~/context/replicate.provider'
+import TransactionProvider from '~/context/transaction.context'
+import { AuthProvider } from '~/context/auth.provider'
+
 import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps<any>) {
   return (
-    <GlobalContextProvider user={pageProps.user || null}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <RootLayout>
-        <ReplicateProvider>
-          <Component {...pageProps} />
-        </ReplicateProvider>
-      </RootLayout>
-    </GlobalContextProvider>
+    <TransactionProvider>
+      <AuthProvider>
+        <GlobalContextProvider>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <RootLayout>
+            <ReplicateProvider>
+              <Component {...pageProps} />
+            </ReplicateProvider>
+          </RootLayout>
+        </GlobalContextProvider>
+      </AuthProvider>
+    </TransactionProvider>
   )
 }
