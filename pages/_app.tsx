6877ea963f6f6226fd21307819cf2ab14e5ef376
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-22 21:36:50 -0600

    chore: fix flow import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,14 @@ import '~/styles/globals.css'
 import '~/styles/wizart-chat.css'
 import '~/styles/home-wizard-steps.css'
 import '~/styles/home-wizard-header.css'
-
-import '../flow/config'
-
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-
 import { AuthProvider } from '~/context/auth.context'
 import { GlobalContextProvider } from '~/context/global.context'
 import { OpenAIProvider } from '~/context/openai.context'
 import { ReplicateProvider } from '~/context/replicate.context'
 import { RootLayout } from '~/layouts/root'
 import '~/styles/globals.css'
-
 import '../config/flow/config'
 
 export default function MyApp({ Component, pageProps }: AppProps<any>) {
