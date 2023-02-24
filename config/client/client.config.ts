e83da3f912e874cc8d1034c02a12331ba955747e
Roberto Lucas <andler.dev@gmail.com>
2023-02-23 19:39:04 -0600

    chore: pub access rm

--- config/client/client.config.ts
+++ config/client/client.config.ts
@@ -1,21 +1,21 @@
 export const clientEnv = {
   debug: process.env.NODE_ENV !== 'production',
   services: {
-    auth: process.env.NEXT_PUBLIC_SERVICE_AUTH || '',
-    replicate: process.env.NEXT_PUBLIC_REPLICATE_API_KEY || '',
-    openai: process.env.NEXT_PUBLIC_OPENAI_API_KEY || '',
+    auth: process.env.SERVICE_AUTH || '',
+    replicate: process.env.REPLICATE_API_KEY || '',
+    openai: process.env.OPENAI_API_KEY || '',
   },
   analytics: {
-    google: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || '',
-    sentry: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
+    google: process.env.GOOGLE_ANALYTICS || '',
+    sentry: process.env.SENTRY_DSN || '',
   },
   cloudinary: {
-    cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || '',
-    apiKey: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || '',
-    apiSecret: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || '',
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
+    apiKey: process.env.CLOUDINARY_API_KEY || '',
+    apiSecret: process.env.CLOUDINARY_API_KEY || '',
   },
   web3auth: {
-    clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '',
-    verifier: process.env.NEXT_PUBLIC_WEB3AUTH_APP_VERIFIER || '',
+    clientId: process.env.WEB3AUTH_CLIENT_ID || '',
+    verifier: process.env.WEB3AUTH_APP_VERIFIER || '',
   },
 }
