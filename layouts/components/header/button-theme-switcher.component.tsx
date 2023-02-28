edd16f9f3503b4a8c04a308715a59726a5674f0d
Roberto Lucas <andre.rlucas@outlook.com>
2023-02-28 16:46:40 -0600

    chore: extra div rm

--- layouts/components/header/button-theme-switcher.component.tsx
+++ layouts/components/header/button-theme-switcher.component.tsx
@@ -7,19 +7,17 @@ export const ButtonThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
 
   return (
-    <div className="flex">
-      <Button
-        variant="transparent"
-        border="sm"
-        className="w-12 h-12 rounded-full border-violet-600 dark:border-yellow-400"
-        onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}
-      >
-        {theme === 'light' ? (
-          <i className="uil uil-moon text-[26px] text-violet-600"></i>
-        ) : (
-          <i className="uil uil-sun text-[26px] text-yellow-400"></i>
-        )}
-      </Button>
-    </div>
+    <Button
+      variant="transparent"
+      border="sm"
+      className="w-12 h-12 rounded-full border-violet-600 dark:border-yellow-400"
+      onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}
+    >
+      {theme === 'light' ? (
+        <i className="uil uil-moon text-[26px] text-violet-600"></i>
+      ) : (
+        <i className="uil uil-sun text-[26px] text-yellow-400"></i>
+      )}
+    </Button>
   )
 }
