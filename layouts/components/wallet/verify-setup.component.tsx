05336ecd4679b053ce0df48d580901c51123cae5
Gabo Esquivel <contact@gaboesquivel.com>
2023-02-28 09:29:29 -0600

    wip: link wallet

--- layouts/components/wallet/verify-setup.component.tsx
+++ layouts/components/wallet/verify-setup.component.tsx
@@ -2,10 +2,12 @@ import { useAsync, useAsyncFn } from 'react-use'
 
 import * as fcl from '@onflow/fcl'
 
-import { Wallet } from '~/graphql/generated/schema'
+import { Wallet, enumWalletState } from '~/graphql/generated/schema'
 import { getContract, getWallets } from '~/services/niftory/niftory-frontend.service'
 
+import { ConfigureWallet } from './configure-wallet.component'
 import { RegisterWallet } from './register-wallet.component'
+import { VerifyWallet } from './veirify-wallet.component'
 import { WalletSetupBox } from './wallet-setup-box.component'
 
 export type WalletSetupStepProps = {
@@ -26,25 +28,25 @@ export function WalletSetup() {
 
   const wallet = getWalletsState.value?.wallet
 
-  console.log('wallet data ==>', wallet)
+  console.log('💰 wallet', wallet)
 
   // User doesn't have a wallet yet
   if (!wallet?.address) return <RegisterWallet />
 
   // User has a wallet but it's not verified yet
-  // if (wallet.state === enumWalletState.UNVERIFIED) return <VerifyWallet blockchain={contractData?.contract?.blockchain} />
+  if (wallet.state === enumWalletState.UNVERIFIED) return <VerifyWallet />
 
   // The user has verified their wallet, but hasn't configured it yet
-  // if (wallet.state === enumWalletState.VERIFIED) return <ConfigureWallet />
+  if (wallet.state === enumWalletState.VERIFIED) return <ConfigureWallet />
 
   // The user has configured their wallet, now they can mint
-  // if (wallet.state === enumWalletState.READY)
-  return (
-    <WalletSetupBox
-      label={`You're are set up! Address is ${wallet?.address}`}
-      error={getWalletsState.error}
-      isLoading={isLoading}
-      onClick={() => {}}
-    />
-  )
+  if (wallet.state === enumWalletState.READY)
+    return (
+      <WalletSetupBox
+        label={`You're are set up! Address is ${wallet?.address}`}
+        error={getWalletsState.error}
+        isLoading={isLoading}
+        onClick={() => {}}
+      />
+    )
 }
