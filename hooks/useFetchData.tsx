846009b2c706b3c2b130fe2d355db99cbc09a7a2
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-02-27 21:16:22 -0600

    chore: improve useFetchData

--- hooks/useFetchData.tsx
+++ hooks/useFetchData.tsx
@@ -1,49 +1,45 @@
 import { useCallback, useEffect, useState } from 'react'
 
+type DependencyArray = readonly unknown[]
 interface ResponseType<OutputType> {
   data: OutputType | null
   isLoading: boolean
   error: Error | null
   fulfilled: boolean
-  refetch: () => Promise<void>
 }
 
 export function useFetchData<OutputType>(
   fetchService: () => Promise<OutputType>,
+  dependencies: DependencyArray = [],
 ): ResponseType<OutputType> {
   const [data, setData] = useState<OutputType | null>(null)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<Error | null>(null)
   const [fulfilled, setFulfilled] = useState(false)
 
-  const fetch = useCallback(async () => {
-    setIsLoading(true)
-    setError(null)
-    setFulfilled(false)
-    try {
-      const response = await fetchService()
-      setData(response)
-      setIsLoading(false)
-      setFulfilled(true)
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setError(error)
-      } else {
-        // handle non-Error objects, if necessary
-        setError(new Error('An unknown error occurred'))
-      }
-    } finally {
-      setIsLoading(false)
-    }
-  }, [fetchService])
-
-  const refetch = useCallback(async () => {
-    await fetch()
-  }, [fetch])
-
   useEffect(() => {
-    fetch()
-  }, [fetch])
+    const fetchData = async () => {
+      setIsLoading(true)
+      setError(null)
+      setFulfilled(false)
+      try {
+        const response = await fetchService()
+        setData(response)
+        setIsLoading(false)
+        setFulfilled(true)
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error)
+        } else {
+          // handle non-Error objects, if necessary
+          setError(new Error('An unknown error occurred'))
+        }
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [dependencies])
 
-  return { data, isLoading, error, fulfilled, refetch }
+  return { data, isLoading, error, fulfilled }
 }
